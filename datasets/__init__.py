4de297958f05c54e368d16015a30a0142cb7e8cd
ShaneXiangH <823473811@qq.com>
2024-12-07 23:09:15 -0500

    update modular BVQA metris

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,5 +3,6 @@ from .toy_dataset import ToyDataset
 from .gstvqa_dataset import GSTVQADataset
 from .clipsim_dataset import CLIPSimDataset
 from .kinetics_dataset import KineticsDataset
+from .konvid_1k_dataset import KONVID1KDataset_ModularBVQA
 
-__all__ = ['ToyDataset', 'GSTVQADataset', 'CLIPSimDataset', 'KineticsDataset']
+__all__ = ['ToyDataset', 'GSTVQADataset', 'CLIPSimDataset', 'KineticsDataset', 'KONVID1KDataset_ModularBVQA']
