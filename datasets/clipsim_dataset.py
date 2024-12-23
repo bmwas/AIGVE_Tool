1474528c1ea3c2face3f2e3ac8cb0b615357429c
Zizhong Li <74041243+lizizhong@users.noreply.github.com>
2024-12-23 09:40:48 -0800

    Update clipsim_dataset.py

--- datasets/clipsim_dataset.py
+++ datasets/clipsim_dataset.py
@@ -5,16 +5,19 @@ import cv2
 import json
 import torch
 from torch.utils.data import Dataset
-from transformers import AutoTokenizer
+from transformers import AutoProcessor, CLIPModel
+from typing import Sequence
 from core.registry import DATASETS
 
 @DATASETS.register_module()
 class CLIPSimDataset(Dataset):
-    def __init__(self, tokenizer_name, video_dir, prompt_dir):
+    def __init__(self, processor_name, video_dir, prompt_dir):
         super(CLIPSimDataset, self).__init__()
+        self.processor_name = processor_name
         self.video_dir = video_dir
         self.prompt_dir = prompt_dir
-        self.tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
+
+        self.processor = AutoProcessor.from_pretrained(self.processor_name)
         self.prompts, self.video_names = self._read_prompt_videoname()
 
     def _read_prompt_videoname(self):
@@ -29,7 +32,7 @@ class CLIPSimDataset(Dataset):
             video_name_list.append(video_name)
 
         return prompt_data_list, video_name_list
-    
+
 
     def __len__(self):
         return len(self.prompts)
@@ -37,7 +40,7 @@ class CLIPSimDataset(Dataset):
     def __getitem__(self, index):
         prompt, video_name = self.prompts[index], self.video_names[index]
         video_path = self.video_dir + video_name
-        frames = []
+        input_frames = []
         cap = cv2.VideoCapture(video_path)
         while cap.isOpened():
             ret, frame = cap.read()
@@ -45,33 +48,37 @@ class CLIPSimDataset(Dataset):
                 break
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             resized_frame = cv2.resize(frame,(224,224))  # Resize the frame to match the expected input size
-            frames.append(resized_frame)
+            input_frames.append(resized_frame)
+        input_frame_tensor= self.processor(
+            images=input_frames,
+            padding=True,
+            truncation=True,
+            max_length=77,
+            return_tensors="pt",
+        )['pixel_values']
+            
+        input_prompt = self.processor(
+            text=[prompt],
+            padding=True,
+            truncation=True,
+            max_length=77,
+            return_tensors="pt",
+        )['input_ids']
 
-        # Convert numpy arrays to tensors, change dtype to float, and resize frames
-        tensor_frames = [torch.from_numpy(frame).permute(2, 0, 1).float() for frame in frames]
+        return input_prompt, input_frame_tensor
 
-        # Tokenize the prompt
-        text_tokens = self.tokenizer(prompt, return_tensors="pt", padding=True, truncation=True, max_length=77)
-
-        # Convert the tokenized text to a tensor and move it to the device
-        prompt_input = text_tokens["input_ids"]
-
-        return prompt_input, tensor_frames
 
 DATASETS.register_module(module=CLIPSimDataset, force=True)
 
-if __name__ == '__main__':
-    tokenizer_name = 'openai/clip-vit-base-patch32'
-    prompt_dir = '/storage/drive_1/zizhong/vqa_toolkit/VQA_Toolkit-main/data/toy/annotations/evaluate.json'
-    video_dir = '/storage/drive_1/zizhong/vqa_toolkit/VQA_Toolkit-main/data/toy/evaluate/'
 
-    clip_dataset = CLIPSimDataset(tokenizer_name=tokenizer_name,
-                               video_dir=video_dir,
-                               prompt_dir=prompt_dir)
-    
-    for index, data in enumerate(clip_dataset):
-        print(index, data)
+# if __name__ == '__main__':
+#     processor_name = 'openai/clip-vit-base-patch32'
+#     clip_model_name = 'openai/clip-vit-base-patch32'
+#     prompt_dir = '/home/exouser/VQA_tool/VQA_Toolkit/data/toy/annotations/evaluate.json'
+#     video_dir = '/home/exouser/VQA_tool/VQA_Toolkit/data/toy/evaluate/'
 
-    
-
-
+#     clip_dataset = CLIPSimDataset(processor_name=processor_name,
+#                                   video_dir=video_dir,
+#                                   prompt_dir=prompt_dir)
+#     clip_dataset.__getitem__(0)
+#     clip_model = CLIPModel.from_pretrained(clip_model_name).to("cuda")
