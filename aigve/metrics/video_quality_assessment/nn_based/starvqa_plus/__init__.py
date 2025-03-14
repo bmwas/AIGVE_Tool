da6b6d9d58062fdcc3b5ad69b34d7ec830ac7fe7
ShaneXiangH <823473811@qq.com>
2025-03-14 12:42:06 -0700

    remove tracking for unused files

--- aigve/metrics/video_quality_assessment/nn_based/starvqa_plus/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) IFM Lab. All rights reserved.
-
-# from .starvqa_plus_metric import StarVQAplus
-# from .StarVQA_PLUS.build.lib.lib.datasets import Kinetics
-# from .StarVQA_PLUS.build.lib.lib.config.defaults import get_cfg
-
-# __all__ = ['StarVQAplus', 'Kinetics', 'get_cfg']
