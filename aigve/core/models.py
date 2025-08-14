535aa291726df4c7c8bd28d9d5eec1b993b7bbe3
Benson <benson.mwangi@gmail.com>
2025-08-14 16:29:04 -0500

    fix model import issues

--- aigve/core/models.py
+++ aigve/core/models.py
@@ -1,7 +1,7 @@
 # Copyright (c) IFM Lab. All rights reserved.
 
 from mmengine.model import BaseModel
-from core import MODELS
+from .registry import MODELS
 import math
 
 @MODELS.register_module()
